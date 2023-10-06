665c1ac80f5d822e61e4014658c23be3204bfbd5
Syed Abid Hassan <abidhassanjaffri31@gmail.com>
2023-10-06 23:26:06 +0500

    Add files via upload

---
+++ Challenge17.py
@@ -0,0 +1,7 @@
+def print_name(first,last):
+    first_name=str(input("Enter a name:"))
+    last_name=str(input("Enter last name:"))
+    x=print("Hello",first_name,last_name + "!","You just delved into python.")
+    return x
+
+print_name("" "")
