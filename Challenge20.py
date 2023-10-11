4afabee068a9eb81add312bc4df5a8f7679caf77
Syed Abid Hassan <abidhassanjaffri31@gmail.com>
2023-10-11 22:11:06 +0500

    Add files via upload

---
+++ Challenge20.py
@@ -0,0 +1,8 @@
+s=input("Enter a string:")
+print(any([char.isalnum() for char in s]))
+print(any([char.isalpha() for char in s]))
+print(any([char.isdigit() for char in s]))
+print(any([char.islower() for char in s]))
+print(any([char.isupper() for char in s]))
+
+print(s.isalpha())
