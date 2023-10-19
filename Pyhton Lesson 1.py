87e17a0998b0a4e182eb25f45cad181873e12348
Syed Abid Hassan <abidhassanjaffri31@gmail.com>
2023-10-20 01:34:05 +0500

    Add files via upload

---
+++ Pyhton Lesson 1.py
@@ -0,0 +1,33 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(2+3)
+5
+print("2+3",2+3)
+2+3 5
+def hello():
+    print("Hello")
+    print("Computers are fun")
+
+    
+hello()
+Hello
+Computers are fun
+def greet(person):
+    print("Hello",person)
+    print("How are you?")
+
+    
+def greet("Abid")
+SyntaxError: invalid syntax
+def greet(Abid)
+SyntaxError: expected ':'
+def greet("Abid"):
+    
+SyntaxError: invalid syntax
+greet("Abid")
+Hello Abid
+How are you?
+print
+<built-in function print>
+greet
+<function greet at 0x0000019D93192560>
